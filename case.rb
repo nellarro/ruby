a7ae4c9266dedeb33f5c42a4425a1b49fb63b7f6
Jenell Pizarro <nellarro@gmail.com>
2017-06-13 23:24:53 -0400

    case statements

---
+++ case.rb
@@ -0,0 +1,9 @@
+current_weather = "stormy"
+case current_weather
+when "stormy"
+puts "bring an umbrella"
+when "sunny"
+puts "wear flip flops and sunscreen"
+when "cloudy"
+puts "deal with it"
+end
