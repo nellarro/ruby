b74e1db115cc6ad06b7c6fac93be8faad68cdebe
Jenell Pizarro <nellarro@gmail.com>
2017-06-13 23:16:36 -0400

    conditional statements. work quicker than js. super cool!

---
+++ if_else.rb
@@ -0,0 +1,9 @@
+fav_color = "teal"
+if (fav_color == "red")
+puts "Red like fire!"
+elseif (fav_color == "orange")
+puts "Orange like, well... an orange."
+elseif (fav_color == "yellow")
+puts "Yellow daffodils are so pretty in the spring!"
+else puts "Hmm. I don't know that color."
+end
