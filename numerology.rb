eb45e2d177f113ef7487bcac5372a16d0e48ed3d
Jenell Pizarro <nellarro@gmail.com>
2017-06-18 19:45:06 -0400

    added refactor rules for numerology

--- numerology.rb
+++ numerology.rb
@@ -11,6 +11,14 @@
 # 1 + 0 = 1
 #
 # Your birth path number is 1!
+
+# REFACTOR:
+# First ask the user for their birthdate. Make sure to set their response to a variable.
+# Then, determine the birth path number inside a method. The method should take the birthdate as an argument. The return value of the method should be the birth path number.
+# Assign the return value from your first method to a variable. You’ll use this variable in step 4.
+# Next figure out the correct message. Create another method that determines what message to display. The method should take the birth path number as an argument. The return value of the method should be the message.
+# Assign the return value from your second method to a variable. You’ll use this variable in step 6.
+# Finally, use puts to show the birth path number and the message (the return value from your second method) to the user.
 puts "Whats your birthdate? (MMDDYYYY)"
 birthdate = gets
 added_numbers = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
--- using_methods.rb
+++ using_methods.rb
@@ -1,7 +1,29 @@
-def math_method(num1, num2)
-  number = num1 + num2
-  puts number
+# def math_method(num1, num2)
+#   number = num1 + num2
+#   puts number
+# end
+#
+# math_method(165, 73)
+# math_method(89, 1234)
+
+# ADDA'S TIME AND GREETING METHOD
+
+def determine_current_hour
+	current_time = Time.new
+	current_hour = current_time.hour
+	return current_hour
 end
 
-math_method(165, 73)
-math_method(89, 1234)
+def greeting(name)
+
+current_hour = determine_current_hour
+
+	if(current_hour > 3 && current_hour < 12) 		time = "morning" 	elsif(current_hour > 12 && current_hour < 18) 		time = "afternoon" 	elsif(current_hour > 18 || current_hour < 2)
+		time = "evening"
+	end
+
+	puts "Good #{time}, #{name.capitalize}!"
+
+end
+
+greeting("Emily")
