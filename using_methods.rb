8e78556d19e181aa3ae0db72204318f956723f44
Jenell Pizarro <nellarro@gmail.com>
2017-06-19 21:27:48 -0400

    i made a method that works and things

--- using_methods.rb
+++ using_methods.rb
@@ -9,21 +9,23 @@
 # ADDA'S TIME AND GREETING METHOD
 
 def determine_current_hour
-	current_time = Time.new
-	current_hour = current_time.hour
-	return current_hour
+	current_hour = Time.new.hour
+end
+
+def ask_name
+	puts "What is your name?"
+	name = gets
 end
 
 def greeting(name)
-
-current_hour = determine_current_hour
-
-	if(current_hour > 3 && current_hour < 12) 		time = "morning" 	elsif(current_hour > 12 && current_hour < 18) 		time = "afternoon" 	elsif(current_hour > 18 || current_hour < 2)
-		time = "evening"
+	current_hour = determine_current_hour
+	if(current_hour > 3 and current_hour < 12)
+		time = "morning"
+	elsif(current_hour > 12 and current_hour < 18)
+		time = "afternoon"
+	elsif(current_hour > 18 or current_hour <2)
+		time = "night"
 	end
-
 	puts "Good #{time}, #{name.capitalize}!"
-
 end
-
-greeting("Emily")
+greeting(ask_name())
